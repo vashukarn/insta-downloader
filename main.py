19e4a0efea3674d0f15f4c97cb06ab06afae07aa
vashukarn <jaykarvashu@gmail.com>
2020-05-01 21:49:18 +0545

    changes

--- main.py
+++ main.py
@@ -4,32 +4,49 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import urllib.request
 
-username = str(input('Enter Username: '))
-driver = webdriver.Chrome()
+# Scrolling to the bottom
+
+
+def scroll(SCROLL_PAUSE_TIME):
+    print('Started Scrolling..')
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+    print('Reached to the end of page')
+
+
+chrome_options = Options()
+# Uncomment two lines down if you don't want chrome window to popup and go headless
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver.minimize_window()
+# Enter tiktok username here
+username = str(input('Enter instagram username : '))
+
 
 driver.get('https://www.instagram.com/' + username + '/')
-
-# for scrolling
-SCROLL_PAUSE_TIME = 1
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(SCROLL_PAUSE_TIME)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-
+scroll(1)
 posts = []
 links = driver.find_elements_by_tag_name('img')
 for link in links:
     post = link.get_attribute('src')
     posts.append(post)
+
+# for removing last instagram image link
 posts.pop()
+
+# closing webdriver
 driver.quit()
+
 print("Links are here \n links Start :: \n")
 for i in range(len(posts)):
-    print(posts[i])
     sleep(2)
     urllib.request.urlretrieve(posts[i], 'media' + str(i) + '.jpg')
+    print('Media' + str(i) + '.jpg downloaded.')
